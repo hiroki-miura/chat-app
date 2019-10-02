91005526b6989318aadffe6f540f94dc519e5224
hiroki-miura <mrbigrabu0221@yahoo.co.jp>
2019-10-02 15:07:21 +0900

    add document & speak content => room.js

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -13,6 +13,16 @@ App.room = App.cable.subscriptions.create("RoomChannel", {
   },
 
   speak: function(content) {
-    return this.perform('speak',{message: "ハロー！"});
+    return this.perform('speak',{message: content});
   }
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+  const input =document.getElementById('chat-input')
+  const button =document.getElementById('button')
+  button.addEventListener('click', function(){
+    const content = input.value
+    App.room.speak(content)
+    input.value = ''
+  })
+})
