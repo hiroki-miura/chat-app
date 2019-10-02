6ea628647cfdcfafd609ca5d0a2f52818c74383b
hiroki-miura <mrbigrabu0221@yahoo.co.jp>
2019-10-02 16:10:57 +0900

    fin

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -14,13 +14,13 @@ App.room = App.cable.subscriptions.create("RoomChannel", {
   },
 
   speak: function(content) {
-    return this.perform('speak',{message: content});
+    return this.perform('speak', {message: content});
   }
 });
 
 document.addEventListener('DOMContentLoaded', function(){
-  const input =document.getElementById('chat-input')
-  const button =document.getElementById('button')
+  const input = document.getElementById('chat-input')
+  const button = document.getElementById('button')
   button.addEventListener('click', function(){
     const content = input.value
     App.room.speak(content)
