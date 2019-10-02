70a4a837bf800ec535cddfbba4759aa20248c15c
hiroki-miura <mrbigrabu0221@yahoo.co.jp>
2019-10-02 09:44:37 +0900

    add room.js

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,17 +1,18 @@
 App.room = App.cable.subscriptions.create("RoomChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
+    console.log('connected')
   },
 
   disconnected: function() {
     // Called when the subscription has been terminated by the server
   },
 
-  received: function(data) {
+  received: function(message) {
     // Called when there's incoming data on the websocket for this channel
   },
 
-  speak: function() {
+  speak: function(content) {
     return this.perform('speak');
   }
 });
